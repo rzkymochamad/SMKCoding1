c3b4bd0072c47cb84820e613ab3fb5badf1a552c
rzkymochamad <rzkymochamad@gmail.com>
2020-05-04 22:25:49 +0700

    First Commit

--- app/src/main/java/com/example/smkcoding1/MainActivity.kt
+++ app/src/main/java/com/example/smkcoding1/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.smkcoding1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSave.setOnClickListener { goToProfilActivity() }
+    }
+
+    private fun goToProfilActivity() {
+        val intent = Intent(this, ProfilActivity::class.java)
+        startActivity(intent)
     }
 }
